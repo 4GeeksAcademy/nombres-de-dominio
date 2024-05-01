59b85b6ab66a2e103367b8989c4934ea962b96b6
Alba <allbaluccia@gmail.com>
2024-05-01 22:04:11 +0000

    prueba

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let pronoun = ["the", "our", "your"];
+let adj = ["great", "big", "nice"];
+let noun = ["jogger", "racoon", "bigger"];
+let domain = [".es", ".com", ".net"];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+for (let i = pronoun; i < pronoun.length; i++) {
+  for (let j = adj; j < adj.length; j++) {
+    for (let x = noun; x < noun.length; x++) {
+      for (let z = domain; z < domain.length; z++) {
+        console.log(pronoun[i] + adj[j] + noun[x] + domain[z]);
+      }
+    }
+  }
+}
